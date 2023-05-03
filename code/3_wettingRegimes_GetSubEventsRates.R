582bc568b3c7c1827df66098d87fe24e74f13de0
Adam Price <adamprice@Adams-MBP-3.home>
2023-05-03 11:11:12 -0700

    Directory rearrange and pushing changes

---
+++ code/3_wettingRegimes_GetSubEventsRates.R
@@ -0,0 +1,172 @@
+#####################################################################
+##
+## Script name: 3_wettingRegimes_GetSubEventsRate.R
+##
+## Author: Adam N. Price
+##
+## Date Created: 2022-02-25
+##
+## Copyright (c) Adam N. Price, 2022
+## Email: adnprice@ucsc.edu
+##
+############################# Description ##########################
+##
+## Code to extract rewetting events at different flow thresholds
+##   22-02-25: Current threholds are Q5,10,15,25,50, and peaks after
+##             those thresholds are met
+##
+############################# Packages #############################
+library(parallel)
+library(lubridate)
+library(tidyverse)
+library(dataRetrieval)
+##   
+##
+############################# Code ################################
+
+# Load wetting date data
+wetEvents = read_csv("data/wettingRegimes_wettingLengths.csv") %>%
+  na.omit()
+
+## Define the function to extract revwetting events
+
+
+metrics_fun <- function(n){
+  library(lubridate)
+  library(tidyverse)
+  
+
+  siteNumber = str_pad(wetEvents$gage[n], 8, pad = "0")
+  parameterCd = "00060"
+  startDate = wetEvents$wet_date[n]-1
+  endDate = wetEvents$wetLengthDate[n]
+  
+  dat = dataRetrieval::readNWISuv(
+    siteNumber,
+    parameterCd,
+    startDate,
+    endDate
+  )
+  
+  dat$q_peak= round(dat$X_00060_00000,1)
+  
+  dat<-dat %>% 
+    #Define forward and backward slope at each point
+    mutate(
+      slp_b = (q_peak-lag(q_peak))/(as.numeric(dat$dateTime-lag(dat$dateTime))), 
+      slp_f = (lead(q_peak)-q_peak)/(as.numeric(lead(dat$dateTime)-dat$dateTime))
+    ) %>% 
+    #now flag those derivative changes
+    # mutate(peak_flag = if_else(slp_b>0.0001 & slp_f<0, 1,0),
+    mutate(peak_flag = if_else(slp_b>=0 & slp_f<0, 1,0),
+           peak_flag = if_else(is.na(peak_flag), 0, peak_flag)) 
+  
+  tt <- dat %>%
+    filter(peak_flag==1) %>%
+    select(site_no,dateTime,X_00060_00000) %>%
+    setNames(c("gage","wet_DateTime","q"))
+  
+  
+  initEvent = dat[dat$dateTime<=tt$wet_DateTime[1] & dat$q_peak>0,]
+  
+  #Create linear model of dQ vs q
+  t<- initEvent %>% mutate(dQ = abs(lag(X_00060_00000) - X_00060_00000)) %>% filter(dQ>=0)
+  model<-lm(log10(dQ+0.1)~log10(X_00060_00000+0.1), data=t)
+  
+  inital_rewetting_rate <- tryCatch(as.numeric(model$coefficients[2]), error = function(e) NA)
+  p_value_i <- tryCatch(summary(model)$coefficients[2,4], error = function(e) NA)
+  
+  entireEvent = dat[dat$dateTime<=tt$wet_DateTime[nrow(tt)] & dat$dateTime>=initEvent$dateTime[1],]
+  
+  t<- entireEvent %>% mutate(dQ = abs(lag(X_00060_00000) - X_00060_00000)) %>% filter(dQ>=0)
+  model<-lm(log10(dQ+0.1)~log10(X_00060_00000+0.1), data=t)
+  
+  rewetting_rate <- tryCatch(as.numeric(model$coefficients[2]), error = function(e) NA)
+  p_value <- tryCatch(summary(model)$coefficients[2,4], error = function(e) NA)
+  
+  output <- dat %>%
+    tibble() %>%
+    filter(peak_flag==1) %>%
+    select(site_no,dateTime,X_00060_00000) %>%
+    setNames(c("gage","wet_DateTime","q")) %>%
+    mutate(rewetting_event = wetEvents$event[n],
+           gage =  wetEvents$gage[n],
+           threshold = wetEvents$Q_threshold[n],
+           weting_subevent = row_number(),
+           total_false_starts = count(.),
+           inital_rewetting_rate = inital_rewetting_rate,
+           p_value_i = p_value_i,
+           rewetting_rate = rewetting_rate,
+           p_value = p_value,
+           sample_rate = as.numeric(difftime(dat$dateTime[2],dat$dateTime[1],units='hours')))
+  
+  tempInit = initEvent[1,] %>%
+    tibble() %>%
+    select(site_no,dateTime,X_00060_00000) %>%
+    setNames(c("gage","wet_DateTime","q")) %>%
+    mutate(rewetting_event = wetEvents$event[n],
+           gage =  wetEvents$gage[n],
+           threshold = wetEvents$Q_threshold[n],
+           weting_subevent = 0,
+           total_false_starts = count(output),
+           inital_rewetting_rate = inital_rewetting_rate,
+           p_value_i = p_value_i,
+           rewetting_rate = rewetting_rate,
+           p_value = p_value,
+           sample_rate = as.numeric(difftime(dat$dateTime[2],dat$dateTime[1],units='hours')))
+  
+  output <- output %>% add_row(tempInit,.before=1)
+  ## Call output
+  output
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Step 3: Execute and write-----------------------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Start timer
+t0<-Sys.time()
+
+#Create error handling function
+execute<-function(a){
+  tryCatch(metrics_fun(a), error=function(e){
+    tibble(
+      gage = NA,
+      threshold = NA,
+      wet_DateTime = NA,
+      q = NA,
+      rewetting_event =NA,
+      weting_subevent = NA,
+      total_false_starts = NA,
+      inital_rewetting_rate = NA,
+      p_value_i = NA,
+      rewetting_rate = NA,
+      p_value = NA,
+      sample_rate = NA)}
+  )
+}
+
+# get number of cores
+n.cores <- detectCores()
+
+#start cluster
+cl <-  makePSOCKcluster(n.cores)
+
+#Export file list to cluster
+clusterExport(cl, c('metrics_fun','wetEvents'), env=.GlobalEnv)
+
+# Use mpapply to exicute function
+x<-parLapply(cl,seq(1, nrow(wetEvents)),execute) #length(files)
+# x<-parLapply(cl,seq(1381, 1385),execute) #length(files)
+
+# Stop the cluster
+stopCluster(cl)
+
+#gather output
+output<-bind_rows(x)
+
+#Capture finishing time
+tf<-Sys.time()
+tf-t0
+
+# Write the data
+write_csv(output,"data/wettingRegimes_SubEvents.csv")
---
+++ code/zz_archive/rewettingRate_test.R
@@ -0,0 +1,183 @@
+library(parallel)
+library(lubridate)
+library(tidyverse)
+
+
+# Get list of files
+files <- list.files('../../DryingRegimes/data/daily_data_with_ climate_and_PET/csv',pattern = "*.csv",full.names = TRUE)
+
+gage <- as.character(tools::file_path_sans_ext(basename(files)))
+
+# Load wetting date data
+wet = read_csv("data/wettingRegimes_wetDate.csv")
+wet$wet_date = lubridate::as_date(wet$wet_date)
+
+# output = tibble(event = NA,
+#                 gage = NA,
+#                 cluster = NA,
+#                 wet_date =NA,
+#                 wetLengthDate = NA,
+#                 wetLength = NA,
+#                 wetLengthQ = NA,
+#                 wetPeak = NA,
+#                 wetPeakLength = NA,
+#                 wetPeakQ = NA,
+#                 Q_threshold = NA)
+
+
+metrics_fun <- function(n){
+  library(lubridate)
+  library(tidyverse)
+  
+  # for (n in 1:nrow(wet)){
+  
+  
+  output = tibble(
+    event = NA,
+    gage = NA,
+    cluster = NA,
+    wet_date =NA,
+    wetLengthDate = NA,
+    wetLength = NA,
+    wetLengthQ = NA,
+    wetPeak = NA,
+    wetPeakLength = NA,
+    wetPeakQ = NA,
+    Q_threshold = NA,
+    rewetting_rate = NA,
+    p_value = NA)
+  
+  
+  whichfile <- grep(
+    x = files,
+    pattern = wet$gage[n],
+    value = TRUE
+  )
+  
+  
+  dat <- read_csv(file = whichfile, 
+                  col_types = 'dDdddddddd') %>% 
+    mutate(date=as_date(Date), 
+           num_date = as.numeric(Date), 
+           q = X_00060_00003) %>% 
+    select(date, num_date, q) %>% 
+    na.omit() 
+  
+  dat$date = lubridate::as_date(dat$date)
+  wet_date = wet$wet_date[n]
+  gage = wet$gage[n]
+  cluster = wet$cluster[n]
+  
+  p_list = c(0.05,0.1,0.15,0.25,0.50)
+  # p_list = 0.25
+  for (i in p_list){
+    df<-dat %>%
+      #Round to nearest tenth
+      mutate(q = round(q, 1)) %>%
+      #1% quantile thresholds
+      mutate(q_peak = if_else(q>quantile(q,i),  q, 0))
+    
+    df<-df %>% 
+      #Define forward and backward slope at each point
+      mutate(
+        slp_b = (q_peak-lag(q_peak))/(num_date-lag(num_date)), 
+        slp_f = (lead(q_peak)-q_peak)/(lead(num_date)-num_date), 
+        slp_f = (lead(q_peak)-q_peak)/(lead(num_date)-num_date),  
+      ) %>% 
+      #now flag those derivative changes
+      mutate(peak_flag = if_else(slp_b>0.000 & slp_f>0, 0,1),
+             peak_flag = if_else(is.na(peak_flag), 0, peak_flag)) 
+    
+    # Filter out any data before wet date
+    temp = df[df$date>=wet_date,]
+    
+    # Determine the wet length date
+    wetLengthDate  = temp[temp$q_peak>0,]$date[1]
+    
+    # Determine the wetting length
+    wetLength =  as.numeric(wetLengthDate - wet_date,units = "days")
+    
+    # Value at that length
+    wetLengthQ = temp[temp$q_peak>0,]$q_peak[1]
+    
+    ## Get the date for the next wetPeak
+    wetPeak = temp[temp$slp_f<0,]$date[1]
+    
+    ## Get the length to the next wetPeak
+    wetPeakLength =  as.numeric(wetPeak - wet_date,units = "days")
+    
+    ## Get the value at that date
+    wetPeakQ = temp[temp$slp_f<0,]$q_peak[1]
+    
+    ## Write the percentile used in filering
+    Q_threshold = i
+    
+    event = n
+    
+    ### Calculate rewetting rate ###
+    #Create linear model of dQ vs q
+    t <- df[df$date>=wet_date & df$date<wetPeak,]
+    # t <- t %>% mutate(dQ = lag(q) - q) %>% filter(dQ>=0)
+    t <- t %>% mutate(dQ = lag(q) - q)
+    model<-lm(log10(dQ+0.1)~log10(q+0.1), data=t)
+    
+    #Estimate drying rate [note the error catch for low slopes]
+    rewetting_rate <- tryCatch(model$coefficients[2], error = function(e) NA)
+    p_value <- tryCatch(summary(model)$coefficients[2,4], error = function(e) NA)
+    
+    
+    # output = output %>% add_row(event,gage,cluster,wet_date,wetLengthDate,wetLength,wetLengthQ,wetPeak,wetPeakLength,wetPeakQ,Q_threshold)
+    output = output %>% add_row(event,gage,cluster,wet_date,wetLengthDate,wetLength,wetLengthQ,wetPeak,wetPeakLength,wetPeakQ,Q_threshold,rewetting_rate,p_value)
+  }
+  ## Call output
+  output
+}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Step 3: Execute and write-----------------------------------------------------
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#Start timer
+t0<-Sys.time()
+
+#Create error handling function
+execute<-function(a){
+  tryCatch(metrics_fun(a), error=function(e){
+    tibble(
+      event = NA,
+      gage = NA,
+      cluster = NA,
+      wet_date =NA,
+      wetLengthDate = NA,
+      wetLength = NA,
+      wetLengthQ = NA,
+      wetPeak = NA,
+      wetPeakLength = NA,
+      wetPeakQ = NA,
+      Q_threshold = NA)}
+  )
+}
+
+# get number of cores
+n.cores <- detectCores()
+
+#start cluster
+cl <-  makePSOCKcluster(n.cores)
+
+#Export file list to cluster
+clusterExport(cl, c('files', 'metrics_fun','wet'), env=.GlobalEnv)
+
+# Use mpapply to exicute function
+x<-parLapply(cl,seq(1, nrow(wet)),execute) #length(files)
+
+# Stop the cluster
+stopCluster(cl)
+
+#gather output
+output<-bind_rows(x)
+
+#Capture finishing time
+tf<-Sys.time()
+tf-t0
+
+# Write the data
+write_csv(output,"data/wettingRegimes_wettingLengths_Peaks.csv")
